09d829908a8590aae3c6cce4a70c568e4cd9e0ad
Shashi Gowda <shashigowda91@gmail.com>
2014-06-20 16:47:49 +0530

    fpswhen with the inefficiencies removed. Doesn't guarantee interval

--- src/timing.jl
+++ src/timing.jl
@@ -4,6 +4,7 @@ using React
 
 export every, fpswhen, fps, timestamp
 
+# Guarrantees interval
 function every(delta::Float64)
     i = Input(time())
     update(timer, status) = push!(i, time())
@@ -12,16 +13,36 @@ function every(delta::Float64)
     return lift(x->x, Float64, i) # prevent push!
 end
 
-function fpswhen(test::Signal{Bool}, delta::Float64)
-    i = Input(time())
-    update(timer, status) = if test.value push!(i, time()) end
-    t = Timer(update)
-    start_timer(t, delta, delta)
-    return lift(x->x, Float64, i) # prevent push!
+# Try to get to freq number of FPS.
+function fpswhen(test::Signal{Bool}, freq::Float64)
+    diff = Input(0.0)
+
+    local delta = 1/freq, t0 = time(),
+          isOn = test.value, wasOn = false
+
+    function update(timer, status)
+        t = time()
+        push!(diff, t-t0)
+        t0 = t
+    end
+
+    local timer = Timer(update)
+    function gate(isOn, t)
+        if isOn
+            if !wasOn t0 = time() end # a restart
+            start_timer(timer, delta, 0)
+        elseif wasOn
+            stop_timer(timer)
+        end
+        wasOn = isOn
+        return t
+    end
+
+    return lift(gate, Float64, test, diff)
 end
 
 function fps(freq::Float64)
-    return every(1/freq)
+    return fpswhen(Input(true), freq)
 end
 
 function timestamp{T}(s::Signal{T})
