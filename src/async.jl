076b14fafd4955ccff7234a12f297b85f8047093
Shashi Gowda <shashigowda91@gmail.com>
2015-08-31 16:48:14 +0530

    require initial value in async and remote map

--- src/async.jl
+++ src/async.jl
@@ -2,38 +2,35 @@
 export remote_map,
        async_map
 
-function async_map(f, inputs...;
-                       init=f(map(value, inputs)...), typ=Any)
-
-    node = Node(typ, init) 
-    map(inputs...; init=init, typ=typ) do args...
-        outer_task = current_task()
-        task = @async begin
-            try
-                args = map(value, inputs)
-                push!(node, f(args...))
-            catch err
-                bt = catch_backtrace()
-                throwto(outer_task, ReactiveException(err, bt))
-            end
-        end
-    end
-    node
-end
-
-function remote_map(f, inputs...;
-                        procid=myid(), init=f(map(value, inputs)...), typ=Any)
+function async_map(f, init, inputs...; typ=typeof(init))
 
     node = Node(typ, init) 
     map(inputs...; init=init, typ=typ) do args...
         outer_task = current_task()
         @async begin
             try
-                args = map(value, inputs)
+                push!(node, f(args...))
+            catch err
+                bt = catch_backtrace()
+                # send exceptions back up to the runner task
+                throwto(outer_task, CapturedException(err, bt))
+            end
+        end
+    end
+    node
+end
+
+function remote_map(f, init, inputs...; typ=typeof(init))
+
+    node = Node(typ, init) 
+    map(inputs...; init=init, typ=typ) do args...
+        outer_task = current_task()
+        @async begin
+            try
                 push!(node, remotecall_fetch(procid, () -> f(args...)))
             catch err
                 bt = catch_backtrace()
-                throwto(outer_task, ReactiveException(err, bt))
+                throwto(outer_task, CapturedException(err, bt))
             end
         end
     end
