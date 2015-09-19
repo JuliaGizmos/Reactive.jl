89c956835af5b46c20211c36b39d4a61d4a6b1a5
Shashi Gowda <shashigowda91@gmail.com>
2015-09-19 11:16:59 +0530

    add kwargs in deprecated methods

--- src/deprecation.jl
+++ src/deprecation.jl
@@ -2,8 +2,8 @@ import Base: consume, foldl, @deprecate
 export lift, consume, foldl, keepwhen, keepif, dropif, dropwhen
 
 @deprecate lift map
-@deprecate consume(f::Union(Function, DataType), s::Signal...) map(f, s...)
-@deprecate foldl(f, x, s::Signal...) foldp(f, x, s...)
+@deprecate consume(f::Union(Function, DataType), s::Signal...;kwargs...) map(f, s...;kwargs...)
+@deprecate foldl(f, x, s::Signal...;kwargs...) foldp(f, x, s...;kwargs...)
 @deprecate keepwhen filterwhen
 @deprecate keepif filter
 @deprecate dropif(f, default, signal) filter(x -> !f(x), default, signal)
