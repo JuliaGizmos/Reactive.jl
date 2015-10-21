069f7afa741a348c45da8663f7ed99f920058648
Yichao Yu <yyc1992@gmail.com>
2015-10-21 08:09:26 -0400

    Fix FactCheck deprecation warning.

--- test/macro.jl
+++ test/macro.jl
@@ -16,28 +16,28 @@ facts("@lift") do
 
         push!(a, 3)
 
-        @fact t1.value => (a.value,)
-        @fact t2.value => (a.value, b.value)
-        @fact l1.value => [a.value]
-        @fact l2.value => [a.value, b.value]
-        @fact c1.value => Any[a.value]
+        @fact t1.value --> (a.value,)
+        @fact t2.value --> (a.value, b.value)
+        @fact l1.value --> [a.value]
+        @fact l2.value --> [a.value, b.value]
+        @fact c1.value --> Any[a.value]
     end
     context("@lift basics") do
-        @fact value(a)^2 => value(b)
+        @fact value(a)^2 --> value(b)
 
         push!(a, 3)
 
-        @fact a.value^2 => b.value
+        @fact a.value^2 --> b.value
     end
 
     # test use in a function
     context("@lift inside a function") do
         k = 3
        # f(a,b) = @lift a + b + 1 + k
-  
+
        # z = f(a,b)
        # push!(a, 4)
-       # @fact a.value^2 + a.value + 4 => z.value
+       # @fact a.value^2 + a.value + 4 --> z.value
     end
 
 end
