64b72f29535243b173ea603c95433670b6dfacf9
Shashi Gowda <shashigowda91@gmail.com>
2015-11-14 17:50:22 +0530

    Fixes to async.jl

--- src/async.jl
+++ src/async.jl
@@ -2,38 +2,49 @@
 export remote_map,
        async_map
 
-function async_map(f, init, inputs...; typ=typeof(init))
+"""
+    tasks, results = async_map(f, init, input...;typ=typeof(init), onerror=Reactive.print_error)
+Spawn a new task to run a function when input signal updates. Returns a signal of tasks and a `results` signal which updates asynchronously with the results. `init` will be used as the default value of `results`. `onerror` is the callback to be called when an error occurs, by default it is set to a callback which prints the error to STDERR. It's the same as the `onerror` argument to `push!` but is run in the spawned task.
+"""
+function async_map(f, init, inputs...; typ=typeof(init), onerror=print_error)
 
     node = Node(typ, init, inputs)
-    map(inputs...; init=init, typ=typ) do args...
+    map(inputs...; init=nothing, typ=Any) do args...
         outer_task = current_task()
         @async begin
             try
-                push!(node, f(args...))
+                x = f(args...)
+                push!(node, x, onerror)
             catch err
-                bt = catch_backtrace()
-                # send exceptions back up to the runner task
-                throwto(outer_task, CapturedException(err, bt))
+                Base.throwto(outer_task, CapturedException(err, catch_backtrace()))
             end
         end
-    end
-    node
+    end, node
 end
 
-function remote_map(f, init, inputs...; typ=typeof(init))
+"""
+    remoterefs, results = remote_map(procid, f, init, input...;typ=typeof(init), onerror=Reactive.print_error)
+
+Spawn a new task on process `procid` to run a function when input signal updates. Returns a signal of remote refs and a `results` signal which updates asynchronously with the results. `init` will be used as the default value of `results`. `onerror` is the callback to be called when an error occurs, by default it is set to a callback which prints the error to STDERR. It's the same as the `onerror` argument to `push!` but is run in the spawned task.
+"""
+function remote_map(procid, f, init, inputs...; typ=typeof(init), onerror=print_error)
 
     node = Node(typ, init, inputs)
-    map(inputs...; init=init, typ=typ) do args...
+
+    node = Node(typ, init, inputs)
+    map(inputs...; init=nothing, typ=Any) do args...
         outer_task = current_task()
+        rref = @spawnat procid begin
+            f(args...)
+        end
         @async begin
             try
-                push!(node, remotecall_fetch(procid, () -> f(args...)))
+                x = fetch(rref)
+                push!(node, x, onerror)
             catch err
-                bt = catch_backtrace()
-                throwto(outer_task, CapturedException(err, bt))
+                Base.throwto(outer_task, CapturedException(err, catch_backtrace()))
             end
         end
-    end
-    node
+    end, node
 end
 
