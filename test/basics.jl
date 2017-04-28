50db67c5b53d0eda285622a23cdeed1b9128f7d9
SimonDanisch <sdanisch@gmail.com>
2017-04-28 11:35:19 +0200

    use factcheck

--- test/basics.jl
+++ test/basics.jl
@@ -9,8 +9,8 @@ number() = round(Int, rand()*1000)
 
 facts("Basic checks") do
     x = Signal(Float32)
-    @test isa(x, Signal{Type{Float32}})
-    
+    @fact isa(x, Signal{Type{Float32}}) --> true
+
     a = Signal(number())
     b = map(x -> x*x, a)
 
