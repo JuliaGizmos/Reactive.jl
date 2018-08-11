c58f7ca5ac8b9df5ca0e6398cf641a4bdca0dbfb
SimonDanisch <sdanisch@gmail.com>
2018-08-11 17:19:52 +0200

    fixes for 0.7

--- src/core.jl
+++ src/core.jl
@@ -48,7 +48,7 @@ if !debug_memory
             push!(nodes, WeakRef(n))
             push!(edges, Int[])
             foreach(p->push!(edges[p.id], id), parents)
-            finalizer(n, schedule_node_cleanup)
+            finalizer(schedule_node_cleanup, n)
             n
         end
     end
@@ -69,7 +69,7 @@ else
             push!(edges, Int[])
             foreach(p->push!(edges[p.id], id), parents)
             nodeset[n] = nothing
-            finalizer(n, log_gc)
+            finalizer(log_gc, n)
             n
         end
     end
@@ -85,8 +85,8 @@ Signal{T}(t::Type{T}; name::String = auto_name!("input")) = Signal(Type{T}, t, n
 function log_gc(n)
     @async begin
         lock(io_lock)
-        print(STDERR, "Signal got gc'd. Creation backtrace:")
-        Base.show_backtrace(STDERR, n.bt)
+        print(stderr, "Signal got gc'd. Creation backtrace:")
+        Base.show_backtrace(stderr, n.bt)
         println(STDOUT)
         unlock(io_lock)
     end
@@ -261,7 +261,7 @@ Signal whose action triggered the error, and `capex` is a
 exception object, and `processed_bt` which is the backtrace of the
 exception.
 
-The default error callback will print the error and backtrace to STDERR.
+The default error callback will print the error and backtrace to stderr.
 """
 function Base.push!(n::Signal, x, onerror = print_error)
     if async_mode[]
@@ -376,7 +376,7 @@ function run_push(pushnode::Signal, val, onerror, dont_remove_dead = false)
                 onerror(pushnode, val, node, CapturedException(err, bt))
             catch err_onerror
                 if isa(err_onerror, MethodError)
-                    println(STDERR, "The syntax for `onerror` has changed, see ?push!")
+                    println(stderr, "The syntax for `onerror` has changed, see ?push!")
                 end
                 rethrow()
             end
@@ -387,7 +387,7 @@ end
 # Default error handler function
 function print_error(pushnode, val, error_node, ex)
     lock(io_lock)
-    io = STDERR
+    io = stderr
     println(io, "Failed to push!")
     print(io, "    ")
     show(io, val)
--- src/time.jl
+++ src/time.jl
@@ -100,7 +100,7 @@ function every_connect(dt, output)
         close(timer)
     end
     timer = Timer(x -> push!(outputref.value, time(), onerror_close_timer), dt, dt)
-    finalizer(output, _->close(timer))
+    finalizer(_->close(timer), output)
     output
 end
 
@@ -151,7 +151,7 @@ function fpswhen_connect(rate, switch, output, name)
 
     fpswhen_runner() # init
     # ensure timer stops when output node is garbage collected
-    finalizer(output, _->close(timer))
+    finalizer(_->close(timer), output)
 end
 
 """
